f30181b7485b3fdeab48f365595841de1c01adda
AntonChernyshov <21Chernyshova@tonbridge-school.org>
2025-02-26 23:14:18 +0000

    setting up framework for GUI

---
+++ src/GUI_Main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+
+
+
+
+main = tk.Tk()
+main.title("GUI")
+main.geometry("800x600")
+
+title = tk.Label(main, text="Robotic Arm Control", font=("Arial", 24))
+title.pack()
+
+
+
+main.mainloop()
